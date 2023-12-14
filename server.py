ecb4cad034b1d54dd8055549c188a5bdfc06b583
CNFeffery <fefferypzy@gmail.com>
2023-12-14 09:52:22 +0800

    fix: 更换优先国内CDN源

--- server.py
+++ server.py
@@ -23,9 +23,10 @@ class CustomDash(dash.Dash):
             scripts = scripts.replace(
                 external_script,
                 '''<script src="{}" onerror='this.remove(); let fallbackScript = document.createElement("script"); fallbackScript.src = "{}"; document.querySelector("head").prepend(fallbackScript);'></script>'''.format(
-                    re.findall('"(.*?)"', external_script)[0],
                     re.findall('"(.*?)"', external_script)[0]
-                    .replace('https://unpkg.com/', 'https://npm.elemecdn.com/')
+                    # .replace('https://unpkg.com/', 'https://npm.elemecdn.com/'),
+                    .replace('https://unpkg.com/', 'https://npm.onmicrosoft.cn/'),
+                    re.findall('"(.*?)"', external_script)[0]
                 )
             )
 
