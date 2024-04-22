39ae951fa923e4d6baf4c3f4c3bd88703b0b3ec1
CNFeffery <fefferypzy@gmail.com>
2024-04-22 20:33:34 +0800

    perf: 更换首选CDN到registry.npmmirror.com

--- server.py
+++ server.py
@@ -3,7 +3,6 @@ import dash
 
 
 class CustomDash(dash.Dash):
-
     def interpolate_index(self, **kwargs):
         # scripts = (
         #     kwargs.pop('scripts').replace(
@@ -14,24 +13,33 @@ class CustomDash(dash.Dash):
 
         # 提取scripts部分符合条件的外部js资源
         external_scripts = re.findall(
-            '(<script src="http.*?"></script>)',
-            scripts
+            '(<script src="http.*?"></script>)', scripts
         )
 
         # 将原有的script标签内容替换为带备用地址错误切换的版本
         for external_script in external_scripts:
-            # 排除fmc被onmicrosoft封禁的情况
-            if 'markdown' not in external_script:
-                scripts = scripts.replace(
-                    external_script,
-                    '''<script src="{}" onerror='this.remove(); let fallbackScript = document.createElement("script"); fallbackScript.src = "{}"; document.querySelector("head").prepend(fallbackScript);'></script>'''.format(
-                        re.findall('"(.*?)"', external_script)[0]
-                        .replace('https://unpkg.com/', 'https://npm.onmicrosoft.cn/'),
-                        re.findall('"(.*?)"', external_script)[0]
-                    )
-                )
+            # 提取当前资源地址
+            origin_script_src = re.findall('"(.*?)"', external_script)[0]
+            # 抽取关键信息
+            library_name, library_version, library_file = re.findall(
+                'com/(.+)@(.+?)/(.+?)$', origin_script_src
+            )[0]
+            # 基于阿里cdn构建新的资源地址
+            new_library_src = f'https://registry.npmmirror.com/{library_name}/{library_version}/files/{library_file}'
 
-        scripts = '''<script>
+            scripts = scripts.replace(
+                external_script,
+                """<script src="{}" onerror='this.remove(); let fallbackScript = document.createElement("script"); fallbackScript.src = "{}"; document.querySelector("head").prepend(fallbackScript);'></script>""".format(
+                    re.findall('"(.*?)"', external_script)[0].replace(
+                        origin_script_src,
+                        new_library_src,
+                    ),
+                    re.findall('"(.*?)"', external_script)[0],
+                ),
+            )
+
+        scripts = (
+            """<script>
 window.onerror = async function(message, source, lineno, colno, error) {
     if (message.includes('is not defined') !== -1) {
         await waitForModules();
@@ -64,9 +72,13 @@ function delay(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 </script>
-''' + scripts
+"""
+            + scripts
+        )
 
-        return super(CustomDash, self).interpolate_index(scripts=scripts, **kwargs)
+        return super(CustomDash, self).interpolate_index(
+            scripts=scripts, **kwargs
+        )
 
 
 app = CustomDash(
@@ -74,11 +86,9 @@ app = CustomDash(
     suppress_callback_exceptions=True,
     update_title=None,
     serve_locally=False,
-    extra_hot_reload_paths=[
-        './documents'
-    ],
+    extra_hot_reload_paths=['./documents'],
     compress=True,
-    assets_ignore='dark.css'
+    assets_ignore='dark.css',
 )
 
 app.title = 'feffery-utils-components在线文档'
