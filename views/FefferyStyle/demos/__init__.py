b1f18dc331bb3c9aae9ebb39cf8c54826cbb4db7
CNFeffery <fefferypzy@gmail.com>
2024-11-28 22:23:31 +0800

    doc: FefferyStyle组件文档页示例迁移及新增 #60

--- views/FefferyStyle/demos/__init__.py
+++ views/FefferyStyle/demos/__init__.py
@@ -16,7 +16,7 @@ def demos_config() -> list:
         {
             'path': 'basic_usage',
             'title': t('基础使用'),
-            'description': None,
+            'description': '通过参数`rawStyle`令指定的**css**代码片段生效。',
         },
     ]
 
--- views/FefferyStyle/demos/basic_usage.py
+++ views/FefferyStyle/demos/basic_usage.py
@@ -1,3 +1,4 @@
+from dash import html
 import feffery_utils_components as fuc
 from dash.dependencies import Component
 
@@ -6,7 +7,32 @@ def render() -> Component:
     """渲染当前演示用例"""
 
     # 构造演示用例相关内容
-    demo_contents = None
+    demo_contents = [
+        fuc.FefferyStyle(
+            rawStyle="""
+.style-demo {
+    background: white;
+    width: 400px;
+    height: 200px;
+    border: 1px solid #f0f0f0;
+    border-radius: 8px;
+    cursor: pointer;
+    transition: 0.3s;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 25px;
+}
+
+.style-demo:hover {
+    transform: scale(1.05);
+    box-shadow: 0px 0px 12px rgba(0, 0, 0, .12);
+    transition: 0.3s;
+}
+"""
+        ),
+        html.Div('鼠标移入查看效果', className='style-demo'),
+    ]
 
     return demo_contents
 
@@ -16,7 +42,33 @@ def code_string() -> list:
 
     return [
         {
-            'code': """
+            'code': '''
+[
+    fuc.FefferyStyle(
+        rawStyle="""
+.style-demo {
+    background: white;
+    width: 400px;
+    height: 200px;
+    border: 1px solid #f0f0f0;
+    border-radius: 8px;
+    cursor: pointer;
+    transition: 0.3s;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 25px;
+}
+
+.style-demo:hover {
+    transform: scale(1.05);
+    box-shadow: 0px 0px 12px rgba(0, 0, 0, .12);
+    transition: 0.3s;
+}
 """
+    ),
+    html.Div('鼠标移入查看效果', className='style-demo'),
+]
+'''
         }
     ]
