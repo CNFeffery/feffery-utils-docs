3a8c3360deee4a051ae35eead42c74577246ec86
CNFeffery <fefferypzy@gmail.com>
2024-12-08 21:48:50 +0800

    doc: FefferyCssVar组件文档页示例迁移及新增 #36

--- views/FefferyCssVar/demos/__init__.py
+++ views/FefferyCssVar/demos/__init__.py
@@ -16,7 +16,7 @@ def demos_config() -> list:
         {
             'path': 'basic_usage',
             'title': t('基础使用'),
-            'description': None,
+            'description': '通过参数`cssVars`管理更新若干**CSS**变量。',
         },
     ]
 
--- views/FefferyCssVar/demos/basic_usage.py
+++ views/FefferyCssVar/demos/basic_usage.py
@@ -1,14 +1,57 @@
+import feffery_antd_components as fac
 import feffery_utils_components as fuc
-from dash.dependencies import Component
+from dash.dependencies import Component, Input, Output
+
+from server import app
 
 
 def render() -> Component:
     """渲染当前演示用例"""
 
     # 构造演示用例相关内容
-    demo_contents = None
+    demo_contents = fac.AntdSpace(
+        [
+            fac.AntdSwitch(
+                id='css-var-demo',
+                checked=False,
+                unCheckedChildren='🌞',
+                checkedChildren='🌛',
+            ),
+            fuc.FefferyCssVar(id='css-var-demo-output'),
+            fac.AntdCenter(
+                'FefferyCssVar示例',
+                style={
+                    'background': 'var(--demo-bg-color)',
+                    'color': 'var(--demo-color)',
+                    'fontSize': '28px',
+                    'transition': '0.2s',
+                    'padding': '100px 150px',
+                    'borderRadius': '5px',
+                },
+            ),
+        ],
+        direction='vertical',
+    )
 
     return demo_contents
+
+
+app.clientside_callback(
+    """(checked) => {
+        if (checked) {
+            return {
+                '--demo-bg-color': 'black',
+                '--demo-color': 'white'
+            }
+        }
+        return {
+            '--demo-bg-color': '#fffbe6',
+            '--demo-color': 'black'
+        }
+    }""",
+    Output('css-var-demo-output', 'cssVars'),
+    Input('css-var-demo', 'checked'),
+)
 
 
 def code_string() -> list:
@@ -16,7 +59,49 @@ def code_string() -> list:
 
     return [
         {
-            'code': """
-"""
+            'code': '''
+fac.AntdSpace(
+    [
+        fac.AntdSwitch(
+            id='css-var-demo',
+            checked=False,
+            unCheckedChildren='🌞',
+            checkedChildren='🌛',
+        ),
+        fuc.FefferyCssVar(id='css-var-demo-output'),
+        fac.AntdCenter(
+            'FefferyCssVar示例',
+            style={
+                'background': 'var(--demo-bg-color)',
+                'color': 'var(--demo-color)',
+                'fontSize': '28px',
+                'transition': '0.2s',
+                'padding': '100px 150px',
+                'borderRadius': '5px',
+            },
+        ),
+    ],
+    direction='vertical',
+)
+
+...
+
+app.clientside_callback(
+    """(checked) => {
+        if (checked) {
+            return {
+                '--demo-bg-color': 'black',
+                '--demo-color': 'white'
+            }
+        }
+        return {
+            '--demo-bg-color': '#fffbe6',
+            '--demo-color': 'black'
+        }
+    }""",
+    Output('css-var-demo-output', 'cssVars'),
+    Input('css-var-demo', 'checked'),
+)
+'''
         }
     ]
