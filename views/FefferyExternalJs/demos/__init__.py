5210fe6bbf19f50da78a43adfe3cb8531e19aec6
CNFeffery <fefferypzy@gmail.com>
2024-12-08 17:15:39 +0800

    doc: FefferyExternalJs组件文档页示例迁移及新增 #40

--- views/FefferyExternalJs/demos/__init__.py
+++ views/FefferyExternalJs/demos/__init__.py
@@ -16,7 +16,7 @@ def demos_config() -> list:
         {
             'path': 'basic_usage',
             'title': t('基础使用'),
-            'description': None,
+            'description': '动态加载指定地址的**js**文件，加载阶段状态可监听。',
         },
     ]
 
--- views/FefferyExternalJs/demos/basic_usage.py
+++ views/FefferyExternalJs/demos/basic_usage.py
@@ -1,14 +1,60 @@
+import dash
+import feffery_antd_components as fac
 import feffery_utils_components as fuc
-from dash.dependencies import Component
+from dash.dependencies import Component, Input, Output
+
+from server import app
 
 
 def render() -> Component:
     """渲染当前演示用例"""
 
     # 构造演示用例相关内容
-    demo_contents = None
+    demo_contents = [
+        fac.AntdSpace(
+            [
+                fac.AntdButton('挂载party.js', id='mount-js'),
+                fac.AntdText(id='mount-js-status'),
+                fac.AntdButton(
+                    '点我试试', id='effect-when-mount', type='primary'
+                ),
+            ],
+            direction='vertical',
+        ),
+        fuc.FefferyExternalJs(id='external-js-demo'),
+        fuc.FefferyExecuteJs(id='execute-party-effect'),
+    ]
 
     return demo_contents
+
+
+app.clientside_callback(
+    '(recentlyStatus) => `recentlyStatus: ${recentlyStatus}`',
+    Output('mount-js-status', 'children'),
+    Input('external-js-demo', 'recentlyStatus'),
+)
+
+
+@app.callback(
+    [
+        Output('external-js-demo', 'jsUrl'),
+        Output('execute-party-effect', 'jsString'),
+    ],
+    Input('mount-js', 'nClicks'),
+    prevent_initial_call=True,
+)
+def external_js_demo(nClicks):
+    if nClicks and nClicks == 1:
+        return [
+            'https://fastly.jsdelivr.net/npm/party-js@latest/bundle/party.min.js',
+            """
+document.querySelector("#effect-when-mount").addEventListener("click", function (e) {
+    party.confetti(this);
+});
+""",
+        ]
+
+    return dash.no_update
 
 
 def code_string() -> list:
@@ -16,7 +62,51 @@ def code_string() -> list:
 
     return [
         {
-            'code': """
-"""
+            'code': '''
+[
+    fac.AntdSpace(
+        [
+            fac.AntdButton('挂载party.js', id='mount-js'),
+            fac.AntdText(id='mount-js-status'),
+            fac.AntdButton(
+                '点我试试', id='effect-when-mount', type='primary'
+            ),
+        ],
+        direction='vertical',
+    ),
+    fuc.FefferyExternalJs(id='external-js-demo'),
+    fuc.FefferyExecuteJs(id='execute-party-effect'),
+]
+
+...
+
+app.clientside_callback(
+    '(recentlyStatus) => `recentlyStatus: ${recentlyStatus}`',
+    Output('mount-js-status', 'children'),
+    Input('external-js-demo', 'recentlyStatus'),
+)
+
+
+@app.callback(
+    [
+        Output('external-js-demo', 'jsUrl'),
+        Output('execute-party-effect', 'jsString'),
+    ],
+    Input('mount-js', 'nClicks'),
+    prevent_initial_call=True,
+)
+def external_js_demo(nClicks):
+    if nClicks and nClicks == 1:
+        return [
+            'https://fastly.jsdelivr.net/npm/party-js@latest/bundle/party.min.js',
+            """
+document.querySelector("#effect-when-mount").addEventListener("click", function (e) {
+    party.confetti(this);
+});
+""",
+        ]
+
+    return dash.no_update
+'''
         }
     ]
