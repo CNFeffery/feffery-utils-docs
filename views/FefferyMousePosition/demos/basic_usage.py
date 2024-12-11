d8f07e6cf57cb2ed4bd71333a27e7a15986d7030
CNFeffery <fefferypzy@gmail.com>
2024-12-11 22:16:24 +0800

    doc: FefferyMousePosition组件文档页示例迁移及新增 #26

--- views/FefferyMousePosition/demos/basic_usage.py
+++ views/FefferyMousePosition/demos/basic_usage.py
@@ -1,14 +1,27 @@
+from dash import html
 import feffery_utils_components as fuc
-from dash.dependencies import Component
+from dash.dependencies import Component, Input, Output
+
+from server import app
 
 
 def render() -> Component:
     """渲染当前演示用例"""
 
     # 构造演示用例相关内容
-    demo_contents = None
+    demo_contents = [
+        fuc.FefferyMousePosition(id='mouse-position-demo'),
+        html.Pre(id='mouse-position-demo-output'),
+    ]
 
     return demo_contents
+
+
+app.clientside_callback(
+    """( position ) => JSON.stringify(position, null, 4)""",
+    Output('mouse-position-demo-output', 'children'),
+    Input('mouse-position-demo', 'position'),
+)
 
 
 def code_string() -> list:
@@ -16,7 +29,19 @@ def code_string() -> list:
 
     return [
         {
-            'code': """
-"""
+            'code': '''
+[
+    fuc.FefferyMousePosition(id='mouse-position-demo'),
+    html.Pre(id='mouse-position-demo-output'),
+]
+
+...
+
+app.clientside_callback(
+    """( position ) => JSON.stringify(position, null, 4)""",
+    Output('mouse-position-demo-output', 'children'),
+    Input('mouse-position-demo', 'position'),
+)
+'''
         }
     ]
