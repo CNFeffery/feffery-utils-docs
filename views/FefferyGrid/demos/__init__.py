cc4285c85e93b2e18ece15b6a3107bd0bfc1882c
CNFeffery <fefferypzy@gmail.com>
2024-12-12 10:08:51 +0800

    doc: FefferyGrid组件文档页示例迁移及新增 #29

--- views/FefferyGrid/demos/__init__.py
+++ views/FefferyGrid/demos/__init__.py
@@ -16,7 +16,7 @@ def demos_config() -> list:
         {
             'path': 'basic_usage',
             'title': t('基础使用'),
-            'description': None,
+            'description': '可拖拽网格基础用例。',
         },
     ]
 
--- views/FefferyGrid/demos/basic_usage.py
+++ views/FefferyGrid/demos/basic_usage.py
@@ -6,7 +6,29 @@ def render() -> Component:
     """渲染当前演示用例"""
 
     # 构造演示用例相关内容
-    demo_contents = None
+    demo_contents = fuc.FefferyGrid(
+        [
+            fuc.FefferyGridItem(
+                str(i),
+                key=str(i),
+                style={
+                    'height': '100%',
+                    'display': 'flex',
+                    'justifyContent': 'center',
+                    'alignItems': 'center',
+                },
+            )
+            for i in range(10)
+        ],
+        layouts=[
+            dict(i=str(i), x=i, y=i + 1, w=1, h=i + i % 2 + 1) for i in range(5)
+        ],
+        cols=5,
+        rowHeight=75,
+        placeholderBorderRadius='5px',
+        margin=[25, 25],
+        style={'border': '1px dashed #e1dfdd'},
+    )
 
     return demo_contents
 
@@ -17,6 +39,29 @@ def code_string() -> list:
     return [
         {
             'code': """
+fuc.FefferyGrid(
+    [
+        fuc.FefferyGridItem(
+            str(i),
+            key=str(i),
+            style={
+                'height': '100%',
+                'display': 'flex',
+                'justifyContent': 'center',
+                'alignItems': 'center',
+            },
+        )
+        for i in range(10)
+    ],
+    layouts=[
+        dict(i=str(i), x=i, y=i + 1, w=1, h=i + i % 2 + 1) for i in range(5)
+    ],
+    cols=5,
+    rowHeight=75,
+    placeholderBorderRadius='5px',
+    margin=[25, 25],
+    style={'border': '1px dashed #e1dfdd'},
+)
 """
         }
     ]
