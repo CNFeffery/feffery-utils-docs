965bbc89d08d2485860a5dd2129f4e80fd2a1197
CNFeffery <fefferypzy@gmail.com>
2024-12-20 22:17:12 +0800

    doc: FefferyGeolocation组件文档页示例迁移及新增 #18

--- views/FefferyGeolocation/demos/basic_usage.py
+++ views/FefferyGeolocation/demos/basic_usage.py
@@ -1,14 +1,29 @@
+import json
+from dash import html
 import feffery_utils_components as fuc
-from dash.dependencies import Component
+from dash.dependencies import Component, Input, Output
+
+from server import app
 
 
 def render() -> Component:
     """渲染当前演示用例"""
 
     # 构造演示用例相关内容
-    demo_contents = None
+    demo_contents = [
+        fuc.FefferyGeolocation(id='geolocation-demo'),
+        html.Pre(id='geolocation-demo-output'),
+    ]
 
     return demo_contents
+
+
+@app.callback(
+    Output('geolocation-demo-output', 'children'),
+    Input('geolocation-demo', 'geoLocationInfo'),
+)
+def geolocation_demo(geoLocationInfo):
+    return json.dumps(geoLocationInfo, ensure_ascii=False, indent=4)
 
 
 def code_string() -> list:
@@ -17,6 +32,19 @@ def code_string() -> list:
     return [
         {
             'code': """
+[
+    fuc.FefferyGeolocation(id='geolocation-demo'),
+    html.Pre(id='geolocation-demo-output'),
+]
+
+...
+
+@app.callback(
+    Output('geolocation-demo-output', 'children'),
+    Input('geolocation-demo', 'geoLocationInfo'),
+)
+def geolocation_demo(geoLocationInfo):
+    return json.dumps(geoLocationInfo, ensure_ascii=False, indent=4)
 """
         }
     ]
