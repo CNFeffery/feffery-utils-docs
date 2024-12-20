7292b3d046a397f6ecf46e59aaa7d7a66ad08203
CNFeffery <fefferypzy@gmail.com>
2024-12-20 21:48:30 +0800

    doc: FefferyKeyPress组件文档页示例迁移及新增 #21

--- views/FefferyKeyPress/demos/basic_usage.py
+++ views/FefferyKeyPress/demos/basic_usage.py
@@ -1,14 +1,60 @@
+import feffery_antd_components as fac
 import feffery_utils_components as fuc
-from dash.dependencies import Component
+from dash.dependencies import Component, Input, Output
+
+from server import app
 
 
 def render() -> Component:
     """渲染当前演示用例"""
 
     # 构造演示用例相关内容
-    demo_contents = None
+    demo_contents = [
+        fac.AntdParagraph(
+            [
+                fac.AntdText('用于监听指定的按键行为，常用的按键别名有：'),
+                *[
+                    fac.AntdText([fac.AntdText(key_name, keyboard=True), '、'])
+                    for key_name in [
+                        'backspace',
+                        'tab',
+                        'enter',
+                        'shift',
+                        'ctrl',
+                        'alt',
+                        'pausebreak',
+                        'capslock',
+                        'esc',
+                        'space',
+                        'pageup',
+                        'pagedown',
+                        'end',
+                        'home',
+                        'leftarrow',
+                        'uparrow',
+                        'rightarrow',
+                        'downarrow',
+                        'insert',
+                        'delete',
+                        'leftwindowkey',
+                        'rightwindowkey',
+                    ]
+                ],
+            ]
+        ),
+        fuc.FefferyKeyPress(id='key-press-demo', keys='ctrl.s'),
+        fac.AntdParagraph(id='key-press-demo-output'),
+    ]
 
     return demo_contents
+
+
+@app.callback(
+    Output('key-press-demo-output', 'children'),
+    Input('key-press-demo', 'pressedCounts'),
+)
+def key_press_demo(pressedCounts):
+    return f'pressedCounts: {pressedCounts}'
 
 
 def code_string() -> list:
@@ -17,6 +63,51 @@ def code_string() -> list:
     return [
         {
             'code': """
+[
+    fac.AntdParagraph(
+        [
+            fac.AntdText('　　用于监听指定的按键行为，常用的按键别名有：'),
+            *[
+                fac.AntdText([fac.AntdText(key_name, keyboard=True), '、'])
+                for key_name in [
+                    'backspace',
+                    'tab',
+                    'enter',
+                    'shift',
+                    'ctrl',
+                    'alt',
+                    'pausebreak',
+                    'capslock',
+                    'esc',
+                    'space',
+                    'pageup',
+                    'pagedown',
+                    'end',
+                    'home',
+                    'leftarrow',
+                    'uparrow',
+                    'rightarrow',
+                    'downarrow',
+                    'insert',
+                    'delete',
+                    'leftwindowkey',
+                    'rightwindowkey',
+                ]
+            ],
+        ]
+    ),
+    fuc.FefferyKeyPress(id='key-press-demo', keys='ctrl.s'),
+    fac.AntdParagraph(id='key-press-demo-output'),
+]
+
+...
+
+@app.callback(
+    Output('key-press-demo-output', 'children'),
+    Input('key-press-demo', 'pressedCounts'),
+)
+def key_press_demo(pressedCounts):
+    return f'pressedCounts: {pressedCounts}'
 """
         }
     ]
