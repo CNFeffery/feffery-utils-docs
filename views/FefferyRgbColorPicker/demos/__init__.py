785c1481b5329ec61ad9d422b02b8ce86d3fe049
CNFeffery <fefferypzy@gmail.com>
2024-12-23 21:56:12 +0800

    doc: FefferyRgbColorPicker组件文档页示例迁移及新增 #13

--- views/FefferyRgbColorPicker/demos/__init__.py
+++ views/FefferyRgbColorPicker/demos/__init__.py
@@ -16,7 +16,7 @@ def demos_config() -> list:
         {
             'path': 'basic_usage',
             'title': t('基础使用'),
-            'description': None,
+            'description': '监听`color`变化获取当前选中颜色。',
         },
     ]
 
--- views/FefferyRgbColorPicker/demos/basic_usage.py
+++ views/FefferyRgbColorPicker/demos/basic_usage.py
@@ -1,14 +1,50 @@
+from dash import html
+import feffery_antd_components as fac
 import feffery_utils_components as fuc
-from dash.dependencies import Component
+from dash.dependencies import Component, Input, Output, State
+
+from server import app
 
 
 def render() -> Component:
     """渲染当前演示用例"""
 
     # 构造演示用例相关内容
-    demo_contents = None
+    demo_contents = fac.AntdSpace(
+        [
+            fuc.FefferyRgbColorPicker(
+                id='rgb-color-picker-demo', showAlpha=True
+            ),
+            html.Div(
+                id='rgb-color-picker-demo-output',
+                style={
+                    'width': '200px',
+                    'height': '200px',
+                    'display': 'flex',
+                    'alignItems': 'center',
+                    'justifyContent': 'center',
+                    'borderRadius': '5px',
+                    'boxShadow': '0px 0px 12px rgba(0, 0, 0, .12)',
+                    'transition': '0.25s',
+                },
+            ),
+        ],
+        size='large',
+    )
 
     return demo_contents
+
+
+@app.callback(
+    [
+        Output('rgb-color-picker-demo-output', 'style'),
+        Output('rgb-color-picker-demo-output', 'children'),
+    ],
+    Input('rgb-color-picker-demo', 'color'),
+    State('rgb-color-picker-demo-output', 'style'),
+)
+def rgb_color_picker_demo(color, old_style):
+    return [{**old_style, 'background': color}, color]
 
 
 def code_string() -> list:
@@ -17,6 +53,40 @@ def code_string() -> list:
     return [
         {
             'code': """
+fac.AntdSpace(
+    [
+        fuc.FefferyRgbColorPicker(
+            id='rgb-color-picker-demo', showAlpha=True
+        ),
+        html.Div(
+            id='rgb-color-picker-demo-output',
+            style={
+                'width': '200px',
+                'height': '200px',
+                'display': 'flex',
+                'alignItems': 'center',
+                'justifyContent': 'center',
+                'borderRadius': '5px',
+                'boxShadow': '0px 0px 12px rgba(0, 0, 0, .12)',
+                'transition': '0.25s',
+            },
+        ),
+    ],
+    size='large',
+)
+
+...
+
+@app.callback(
+    [
+        Output('rgb-color-picker-demo-output', 'style'),
+        Output('rgb-color-picker-demo-output', 'children'),
+    ],
+    Input('rgb-color-picker-demo', 'color'),
+    State('rgb-color-picker-demo-output', 'style'),
+)
+def rgb_color_picker_demo(color, old_style):
+    return [{**old_style, 'background': color}, color]
 """
         }
     ]
