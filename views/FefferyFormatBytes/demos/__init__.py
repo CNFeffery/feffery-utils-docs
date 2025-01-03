e482a75ab71e83f91f6dfd13a1e3995bdef2c01d
CNFeffery <fefferypzy@gmail.com>
2025-01-03 16:15:55 +0800

    doc: FefferyFormatBytes组件文档页构建 #108

--- views/FefferyFormatBytes/demos/__init__.py
+++ views/FefferyFormatBytes/demos/__init__.py
@@ -16,7 +16,7 @@ def demos_config() -> list:
         {
             'path': 'basic_usage',
             'title': t('基础使用'),
-            'description': None,
+            'description': '针对参数`value`设定的值进行字节格式化。',
         },
     ]
 
--- views/FefferyFormatBytes/demos/basic_usage.py
+++ views/FefferyFormatBytes/demos/basic_usage.py
@@ -1,3 +1,4 @@
+import feffery_antd_components as fac
 import feffery_utils_components as fuc
 from dash.dependencies import Component
 
@@ -6,7 +7,17 @@ def render() -> Component:
     """渲染当前演示用例"""
 
     # 构造演示用例相关内容
-    demo_contents = None
+    demo_contents = fac.AntdSpace(
+        [
+            fuc.FefferyFormatBytes(value=1),
+            fuc.FefferyFormatBytes(value=1024),
+            fuc.FefferyFormatBytes(value=1024 * 1000),
+            fuc.FefferyFormatBytes(value=1024 * 1000 * 1000),
+            fuc.FefferyFormatBytes(value=1024 * 1000 * 1000 * 1000),
+            fuc.FefferyFormatBytes(value=1024 * 1000 * 1000 * 1000 * 1000),
+        ],
+        direction='vertical',
+    )
 
     return demo_contents
 
@@ -17,6 +28,16 @@ def code_string() -> list:
     return [
         {
             'code': """
+fac.AntdSpace(
+    [
+        fuc.FefferyFormatBytes(value=1024),
+        fuc.FefferyFormatBytes(value=1024 * 1000),
+        fuc.FefferyFormatBytes(value=1024 * 1000 * 1000),
+        fuc.FefferyFormatBytes(value=1024 * 1000 * 1000 * 1000),
+        fuc.FefferyFormatBytes(value=1024 * 1000 * 1000 * 1000 * 1000),
+    ],
+    direction='vertical',
+)
 """
         }
     ]
